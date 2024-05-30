54be584ee9b87f4ec23cbe52097f2599801328bf
arkaghosh82 <arkaghosh9832@gmail.com>
2024-05-30 18:24:35 +0530

    starting

---
+++ video54/new.js
@@ -0,0 +1,4 @@
+console.log("Hello World")
+console.log("Lets be a good boy.")
+console.log("The ultimate truth is death.")
+console.log("Now I am becoming death.")
---
+++ video54/script.js
@@ -0,0 +1,12 @@
+alert("hello world")
+console.log("code is running")
+
+var a= prompt("Enter your number: ")
+var istrue=confirm("Are you sure to blast your computer ?")
+if(istrue){
+    console.log("blasting your computer")
+}
+else{
+    console.log("not blasting")
+}
+console.log("Your number is " +a)
